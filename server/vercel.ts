debdfbe7024fd85bea63ef4ca393672a6f2b9642
Cole Patterson <eloc333@gmail.com>
2026-09-23 17:31:54 -0700

    Remove unused Vercel helpers

--- server/vercel.ts
+++
@@ -1,8 +0,0 @@
-import type { IncomingMessage, ServerResponse } from 'node:http'
-import { createApp } from './app'
-
-const app = createApp()
-
-export default function handler(request: IncomingMessage, response: ServerResponse) {
-  app(request, response)
-}
